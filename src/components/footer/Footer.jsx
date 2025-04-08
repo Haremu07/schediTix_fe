e6b31a4a6514a677168206c3fbc341299dc63191
Taofik Ahmed <tafikahmed007@example.com>
2025-04-08 16:11:11 +0100

    added header

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,125 @@
 import React from 'react'
+import "../footer/footer.css"
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhone,
+  FaClock,
+  FaTwitter,
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+} from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+       <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-columns">
+          <div className="footer-column">
+            <h3 className="footer-heading">Quick links</h3>
+            <ul className="footer-links">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Explore Categories</a>
+              </li>
+              <li>
+                <a href="#">Event Categories</a>
+              </li>
+              <li>
+                <a href="#">Subscription plan</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3 className="footer-heading">Support</h3>
+            <ul className="footer-links">
+              <li>
+                <a href="#">Account</a>
+              </li>
+              <li>
+                <a href="#">Faq</a>
+              </li>
+              <li>
+                <a href="#">Legal</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3 className="footer-heading">Others</h3>
+            <ul className="footer-links">
+              <li>
+                <a href="#">Partners</a>
+              </li>
+              <li>
+                <a href="#">Community program</a>
+              </li>
+              <li>
+                <a href="#">conferences</a>
+              </li>
+              <li>
+                <a href="#">affiliate program</a>
+              </li>
+              <li>
+                <a href="#">Subscription plan</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3 className="footer-heading">Need any help?</h3>
+            <ul className="footer-contact">
+              <li>
+                <FaMapMarkerAlt className="footer-icon" />
+                <span>Office address: 204/205 muyibi street, Ajegunle, Olodi Apapa, Lagos, Nigeria.</span>
+              </li>
+              <li>
+                <FaEnvelope className="footer-icon" />
+                <span>Email us: cynthiadymonno04@gmail.com</span>
+              </li>
+              <li>
+                <FaPhone className="footer-icon" />
+                <span>Phone number: +2348041814551</span>
+              </li>
+              <li>
+                <FaClock className="footer-icon" />
+                <span>Office hours: 24hrs, Monday - Sunday</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p className="footer-copyright">© 2024 SchedTrix, Inc. All rights reserved.</p>
+          <div className="footer-social">
+            <a href="#">
+              <FaTwitter />
+            </a>
+            <a>
+              <FaLinkedin />
+            </a>
+            <a href="#">
+              <FaFacebook />
+            </a>
+            <a href="#">
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+        </div>
+        </footer>
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import '../header/header.css';
+import {FiSearch } from 'react-icons/fi';
+import { useNavigate } from 'react-router';
+import orangelogo from '../../assets/orangelogo.png'
 
 const Header = () => {
+ const  navigate = useNavigate();
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header-container">
+          <img className="logo-link"src={orangelogo} alt="" onClick={() => navigate("/")} />
+        <nav className="nav">
+          <a href="/event-categories" className="nav-link">
+            Event Categories
+          </a>
+        </nav>
 
-export default Header
+        <div className="search-box">
+          <input type="text" placeholder="Search events (tech,comedy,concerts...)" />
+          <FiSearch className="search-icon" />
+        </div>
+
+        <div className="actions">
+          <button className="auth-button" 
+          onClick={() => navigate("/login")}
+          >Sign In</button>
+          <button className="auth-button" 
+          onClick={() => navigate("/register")}
+          >Sign Up</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
