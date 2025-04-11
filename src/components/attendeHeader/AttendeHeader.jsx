0a23efc08dc918d79a7f38c0582f0c32e2e8b72d
Taofik Ahmed <tafikahmed007@example.com>
2025-04-11 11:59:35 +0100

    dashboard layout

--- src/components/attendeHeader/AttendeHeader.jsx
+++ src/components/attendeHeader/AttendeHeader.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import orangelogo from "../../assets/orangelogo.png"
 import "./headerAttendee.css"
+import CircleImg from "../../assets/Frame 129.png"
 
 const AttendeHeader = () => {
   return (
     <div className='headerAttende'>
-      AttendeHeader
+      <div className="headerAttendeImgBox">
+      <img className="headerAttendeImg" src={orangelogo} alt="" />
+      </div>
+      <h2 className="headerAttendeh2">Welcome,     Cynthia</h2>
+      <div className="headerAttendeProfile">
+        <div className="headerAttendeProfileCircle">
+          <img className="headerAttendeProfileCircleImg" src={CircleImg} alt="" />
+        </div>
+        <p>Cynthia Chidera</p>
+      </div>
     </div>
   )
 }
