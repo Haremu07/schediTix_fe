98d7e33d3f9cd916cc4e39a72a4951777f18208f
Etopia1 <jolaetopia81@gmail.com>
2025-04-11 22:59:16 +0100

    hh

--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -1,14 +1,9 @@
 import "./resetPassword.css";
 import { GiDialPadlock } from "react-icons/gi";
-<<<<<<< HEAD
 import { useNavigate, useParams } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa6";
 import { useState } from "react";
 import orangeLogo from "../../assets/orangelogo.png";
-=======
-import { useNavigate } from "react-router";
-import orangeLogo from "../../assets/orangelogo.png"
->>>>>>> 7d1f4abb9faced9b914d5f40439750efa58ea68a
 
 const ResetPassword = () => {
   const navigate = useNavigate();
@@ -82,7 +77,6 @@ const ResetPassword = () => {
       <div className="ResetPassword-body">
         <div className="ResetPassword-form">
           <div className="ResetPassword-form-Header">
-<<<<<<< HEAD
             <span className="Back" onClick={() => navigate("/reset-password")}>
               <FaArrowLeft /><p>go back</p>
             </span>
@@ -131,23 +125,6 @@ const ResetPassword = () => {
               </button>
             </form>
           )}
-=======
-        {/* <span className="Back" onClick={() => navigate("/reset-password")}><FaArrowLeft/><p>go back</p></span> */}
-          <h2>Set your new password</h2>
-          <p>Choose a strong password to secure your account</p>
-          </div>
-          <form className="form">
-          <span className="input">
-                     <GiDialPadlock/>
-                    <input type="password" className="input2" placeholder="enther your password"/>
-             </span>
-          <span className="input">
-                     <GiDialPadlock/>
-                    <input type="password" className="input2" placeholder="enther your password"/>
-             </span>
-            <button type="submit" className="btn" onClick={()=> navigate("/password-reset-successFull")}>Save Password</button>
-          </form>
->>>>>>> 7d1f4abb9faced9b914d5f40439750efa58ea68a
         </div>
       </div>
       <div className="Nav2">
