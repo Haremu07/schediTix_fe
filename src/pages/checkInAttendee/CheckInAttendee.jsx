f1a617cd273bd53db2c5c916c5eae53039951735
Agu Chukwuebuka Ezekiel <aguchukwuebuka03@gmail.com>
2025-04-13 09:13:11 -0700

    checkinEvent

--- src/pages/checkInAttendee/CheckInAttendee.jsx
+++ src/pages/checkInAttendee/CheckInAttendee.jsx
@@ -1,8 +1,17 @@
-
+import React from 'react'
+import "./checkInAttendee.css"
 
 const CheckInAttendee = () => {
   return (
-    <div>CheckInAttendee</div>
+    <div className='checkInAttendee'>
+      <nav className='checkInAttendee__nav'>
+        <p style={{color:"black"}}><b>All events</b></p>
+        <p>upcoming events</p>
+         <p>ongoing event</p>
+        <p>ended event</p>
+        <input type="text" placeholder="search for event" className='navInpit'/>
+      </nav>
+    </div>
   )
 }
 
