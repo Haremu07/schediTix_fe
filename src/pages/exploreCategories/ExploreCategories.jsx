5cfcb67e330d1407f9aa079ce016a83949253a50
Etopia1 <jolaetopia81@gmail.com>
2025-04-18 20:00:07 +0100

    mobile

--- src/pages/exploreCategories/ExploreCategories.jsx
+++ src/pages/exploreCategories/ExploreCategories.jsx
@@ -1,33 +1,36 @@
 import "./exploreCategories.css"
-import ExploreFetures from './ExploreFetures'
-import mic from "../../assets/mic.jpg" 
+import ExploreFeatures from "./ExploreFetures"
+import mic from "../../assets/mic.jpg"
 
 const ExploreCategories = () => {
   return (
-    <div className='exploreMainPage'>
-        <nav className='exploreLandingPage'>
-          <div className='exploreMiddleBox'>
-              <section className='exlporeMiddleTop'>
-              <h1>Shows</h1>
-            <div  className="exploreReplaceIn"></div>
-            <h1 style={{color:"#271b6b"}}>Comedy Show</h1>
-              </section>
+    <div className="exploreMainPage">
+      <nav className="exploreLandingPage">
+        <div className="exploreMiddleBox">
+          <section className="exlporeMiddleTop">
+            <h1>Shows</h1>
+            <div className="exploreReplaceIn"></div>
+            <h1 style={{ color: "#271b6b" }}>Comedy</h1>
+          </section>
 
+          <div className="exploreMiddleCenter">
+            <div className="explorelistOfShowsBox active">Mykeal wise live</div>
+            <div className="explorelistOfShowsBox">MC Savio show</div>
+            <div className="explorelistOfShowsBox">Rabboski comedy sh</div>
+          </div>
 
-           
-           <nav className='billBord'>
-            <section className='fadedBlackBox'>
-              <img src={mic} alt="" className="billBordImg"/>
+          <nav className="billBord">
+            <section className="fadedBlackBox">
+              <img src={mic || "/placeholder.svg"} alt="" className="billBordImg" />
               <div className="exploreBillBordText">
                 <p>Comedy Shows in</p>
                 <p>Lagos</p>
-            </div>
+              </div>
             </section>
-           
           </nav>
-          </div>
-        </nav>
-        <ExploreFetures/>
+        </div>
+      </nav>
+      <ExploreFeatures />
     </div>
   )
 }
--- src/pages/exploreCategories/ExploreFetures.jsx
+++ src/pages/exploreCategories/ExploreFetures.jsx
@@ -1,196 +1,123 @@
-import React from 'react'
 import "./exploreCategories.css"
-import { MdLocationOn } from "react-icons/md";
-import { CiHeart } from "react-icons/ci";
+import { MdLocationOn } from "react-icons/md"
+import { CiHeart } from "react-icons/ci"
 
-
-const ExploreFetures = () => {
+const ExploreFeatures = () => {
   return (
-    <div className='exploreFeatureMain'>
-        <nav className='explorePaddingBox'>
-          <div>
-        <p><b>Feature Event</b></p>
+    <div className="exploreFeatureMain">
+      <nav className="explorePaddingBox">
+        <div>
+          <p>
+            <b>Featured Events</b>
+          </p>
+          <div className="featureUnderline"></div>
         </div>
-        <section className='exploreCardBox'>
-        <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
+        <section className="exploreCardBox">
+          <section className="exploreFeturesCards">
+            <section className="exploreimageBox">
+              <CiHeart style={{ fontSize: "20px", margin: "15px", color: "white" }} />
+              <img src="/placeholder.svg?height=180&width=300" alt="" />
             </section>
+            <nav className="elpoerecardFirsBox">
+              <h2>Ajegunle City Youth Marathon</h2>
+              <div>
+                <p>
+                  <b>18th</b>
+                </p>
+                <p>
+                  <b>November,</b>
+                </p>
+                <p>
+                  <b>2025</b>
+                </p>
+              </div>
+            </nav>
+            <nav className="explorecardSecondBox">
+              <p>The Aj City Youth Marathon celebrates the resilience of the ajegunle youth</p>
+            </nav>
+            <nav className="exploreaddress">
+              <MdLocationOn style={{ fontSize: "20px" }} />
+              <p>Eko Hotel and Suites</p>
+            </nav>
+            <nav className="cardThirdBox">
+              <p>
+                <b>₦20,000</b>
+              </p>
+              <span>Buy Ticket</span>
+            </nav>
+          </section>
 
-            <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
+          <section className="exploreFeturesCards">
+            <section className="exploreimageBox">
+              <CiHeart style={{ fontSize: "20px", margin: "15px", color: "white" }} />
+              <img src="/placeholder.svg?height=180&width=300" alt="" />
             </section>
+            <nav className="elpoerecardFirsBox">
+              <h2>Ajegunle Love Feast</h2>
+              <div>
+                <p>
+                  <b>23rd</b>
+                </p>
+                <p>
+                  <b>December,</b>
+                </p>
+                <p>
+                  <b>2025</b>
+                </p>
+              </div>
+            </nav>
+            <nav className="explorecardSecondBox">
+              <p>This is the 4th edition of the biggest community love feast.</p>
+            </nav>
+            <nav className="exploreaddress">
+              <MdLocationOn style={{ fontSize: "20px" }} />
+              <p>Abayomi Hall</p>
+            </nav>
+            <nav className="cardThirdBox">
+              <p>
+                <b>₦20,000</b>
+              </p>
+              <span>Buy Ticket</span>
+            </nav>
+          </section>
 
-            <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
+          <section className="exploreFeturesCards">
+            <section className="exploreimageBox">
+              <CiHeart style={{ fontSize: "20px", margin: "15px", color: "white" }} />
+              <img src="/placeholder.svg?height=180&width=300" alt="" />
             </section>
-
-            <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
-            </section>
-
-            <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
-            </section>
-
-            <section className='exploreFeturesCards'>
-              <section className='exploreimageBox'>
-              <CiHeart style={{fontSize:"20px", margin:"15px"}}/>
-                <img src="" alt="" />
-              </section>
-              <nav className='elpoerecardFirsBox'>
-                <h1>CONVERGENE OF <br />STARS</h1>
-                <div>
-                  <p><b>21st</b></p>
-                  <p><b>September</b></p>
-                  <p><b>2025</b></p>
-                </div>
-              </nav>
-              <nav className='explorecardSecondBox'>
-                <p>This is the 4th edition of the biggest community</p>
-                <p>award ceremony in Africa</p>
-              </nav>
-              <nav className='exploreaddress'>
-              <MdLocationOn style={{fontSize:"20px"}}/>
-                <p>Eko Hotel and Suites</p>
-              </nav>
-              <nav className='cardThirdBox'>
-                <p><b>N 20,000</b></p>
-                <span>
-                  Buy Tickets
-                </span>
-              </nav>
-            </section>
-        
-        
+            <nav className="elpoerecardFirsBox">
+              <h2>CONVERGENCE OF STARS</h2>
+              <div>
+                <p>
+                  <b>21st</b>
+                </p>
+                <p>
+                  <b>September,</b>
+                </p>
+                <p>
+                  <b>2025</b>
+                </p>
+              </div>
+            </nav>
+            <nav className="explorecardSecondBox">
+              <p>This is the 4th edition of the biggest community awards ceremony in Africa</p>
+            </nav>
+            <nav className="exploreaddress">
+              <MdLocationOn style={{ fontSize: "20px" }} />
+              <p>Eko Hotel and Suites</p>
+            </nav>
+            <nav className="cardThirdBox">
+              <p>
+                <b>₦20,000</b>
+              </p>
+              <span>Buy Ticket</span>
+            </nav>
+          </section>
         </section>
-        </nav>
+      </nav>
     </div>
   )
 }
 
-export default ExploreFetures
+export default ExploreFeatures
